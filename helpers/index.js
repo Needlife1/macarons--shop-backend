a846e114340081fe79f2b21e6024357c74b9e418
Visotsky Maxim <needlife124@gmail.com>
2024-07-24 12:38:05 +0300

    test

--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,6 @@
 export { default as validateBody } from "./validateBody.js";
-export { default as HttpError } from "./HttpError.js";
+export { HttpError } from "./HttpError.js";
 export { ctrlWrapper } from "./ctrlWrapper.js";
 export { handleMongooseError } from "./handleMongooseError.js";
 export { default as cloudinary } from "./cloudinary.js";
-export {getPublicIdFromUrl} from "./getPublicIdFromUrl.js"
+export {getPublicIdFromUrl} from "./getPublicIdFromUrl.js";
