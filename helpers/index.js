83bf1cca334baf87898aa7b9574036430c5cec04
Visotsky Maxim <needlife124@gmail.com>
2024-07-24 16:05:20 +0300

    test2

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,5 @@
 export { default as validateBody } from "./validateBody.js";
-export { HttpError } from "./HttpError.js";
+export { HttpError } from "./httpError.js";
 export { ctrlWrapper } from "./ctrlWrapper.js";
 export { handleMongooseError } from "./handleMongooseError.js";
 export { default as cloudinary } from "./cloudinary.js";
